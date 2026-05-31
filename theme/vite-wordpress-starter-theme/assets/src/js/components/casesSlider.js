3bea898259198cd7cb2baee30d81d887f4d81524
Sunder23 <kravchukmax85@gmail.com>
2026-05-31 19:30:23 +0300

    feat: add cases slider component and front-page styles

--- theme/vite-wordpress-starter-theme/assets/src/js/components/casesSlider.js
+++ theme/vite-wordpress-starter-theme/assets/src/js/components/casesSlider.js
@@ -15,7 +15,7 @@ export function initCasesSlider() {
 		modules: [Navigation, Pagination],
 		slidesPerView: 1,
 		spaceBetween: 0,
-		loop: true,
+		// loop: true,
 		navigation: {
 			prevEl: '#casePrev',
 			nextEl: '#caseNext',
